644e2d82eb230166389bd1c25cc23f83ee5458de
crizth135 <cristhian.700.1mail.com>
2021-12-26 21:31:25 -0500

    sesion usuario

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -4,16 +4,36 @@ class ControladorUsuarios{
 
     public function ctrIngresoUsuario(){
         if(isset($_POST["ingUsuario"])){
-          if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"]) &&
-              preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])){
-               $encriptar = crypt($_POST["ingPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
-               $tabla = "usuario";
-               $item = "login";
-               $valor = $_POST["ingUsuario"];
-               $respuesta = ModeloUsuarios::MdlMostrarusuarios ($tabla, $item, $valor);
+            if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"]) &&
+                preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])){
+                $encriptar = ($_POST["ingPassword"]);
+                $tabla = "usuario";
+                $item = "login";
+                $valor = $_POST["ingUsuario"];
+                $respuesta = ModeloUsuarios::MdlMostrarusuarios ($tabla, $item, $valor);
+                
+                if(is_array($respuesta) && $respuesta["login"] == $_POST["ingUsuario"] && $respuesta["clave"] == $encriptar){
 
-            
-              }
+                    $_SESSION["iniciarSesion"] = "ok";
+
+                    echo '
+                        <scrip>
+                        
+                            window.location = "inicio";
+                        </scrip>
+                    
+                    
+                    ';
+
+                }else{
+
+                    
+
+                    echo '<div class="alert alert-danger" > el usuario no esta retgistrado </div>';
+
+                }
+                
+            }
         }
     }
 }
