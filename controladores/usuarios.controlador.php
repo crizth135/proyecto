72996ee7911901736f833ab154e747a68aee32ef
crizth135 <cristhian.700.1mail.com>
2021-12-26 19:19:53 -0500

    prueba

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -12,53 +12,7 @@ class ControladorUsuarios{
                $valor = $_POST["ingUsuario"];
                $respuesta = ModeloUsuarios::MdlMostrarusuarios ($tabla, $item, $valor);
 
-               if(is_array($respuesta) && $respuesta["usuario"] == $_POST["ingUsuario"] && $respuesta["password"] == $encriptar){
-
-                if($respuesta["estado"] == 1){
-
-                    
-
-                    /*=============================================
-                    REGISTRAR FECHA PARA SABER EL ÚLTIMO LOGIN
-                    =============================================*/
-
-                    date_default_timezone_set('America/Bogota');
-
-                    $fecha = date('Y-m-d');
-                    $hora = date('H:i:s');
-
-                    $fechaActual = $fecha.' '.$hora;
-
-                    $item1 = "ultimo_login";
-                    $valor1 = $fechaActual;
-
-                    $item2 = "id";
-                    $valor2 = $respuesta["id"];
-
-                    $ultimoLogin = ModeloUsuarios::mdlActualizarUsuario($tabla, $item1, $valor1, $item2, $valor2);
-
-                    if($ultimoLogin == "ok"){
-
-                        echo '<script>
-
-                            window.location = "inicio";
-
-                        </script>';
-
-                    }				
-                    
-                }else{
-
-                    echo '<br>
-                        <div class="alert alert-danger">El usuario aún no está activado</div>';
-
-                }		
-
-            }else{
-
-                echo '<br><div class="alert alert-danger">Error al ingresar, vuelve a intentarlo</div>';
-
-            }
+            
               }
         }
     }
